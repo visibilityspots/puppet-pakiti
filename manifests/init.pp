a52e485d70be0d161dbfcae9d42b4ae099ddef28
Jan Collijs <jan.collijs@inuits.eu>
2014-01-24 12:45:05 +0100

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,33 @@
+# Class: pakiti
+#
+# Initialization class for the pakiti service
+class pakiti (
+  $motd             = $pakiti::params::motd,
+
+  $client           = $pakiti::params::client,
+  $server           = $pakiti::params::server,
+
+  $servers_name     = $pakiti::params::servers_name,
+  $server_url       = $pakiti::params::server_url,
+  $server_tag       = $pakiti::params::server_tag,
+
+  $server_hostname  = $pakiti::params::server_hostname,
+  $server_dbname    = $pakiti::params::server_dbname,
+  $server_username  = $pakiti::params::server_username,
+  $server_password  = $pakiti::params::server_password,
+
+  $server_web_url   = $pakiti::params::server_web_url,
+  $server_web_title = $pakiti::params::server_web_title,
+
+) inherits pakiti::params {
+
+  if $motd {
+    motd::register{'pakiti':}
+  }
+
+  include pakiti::install
+  include pakiti::config
+
+  Class['pakiti::install'] ->
+  Class['pakiti::config']
+}
---
+++ manifests/params.pp
@@ -0,0 +1,22 @@
+# Class pakiti::params
+#
+# This class contains every parameter which will be used in this puppet module
+# for setting up the pakiti agent
+class pakiti::params {
+  $motd             = false
+
+  $server           = false
+  $client           = true
+
+  $servers_name     = 'pakiti.server.com:443'
+  $server_url       = '/feed/'
+  $server_tag       = 'TEST'
+
+  $server_hostname  = 'localhost'
+  $server_dbname    = 'pakiti'
+  $server_username  = 'pakiti_server'
+  $server_password  = 'mysql_password'
+
+  $server_web_url   = 'https://pakiti.cern.ch'
+  $server_web_title = 'My_Organization'
+}
