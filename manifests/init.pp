a6db52aa399f903d5d19d74e663b7cd0d4c65954
honza <honza@inuits.eu>
2015-09-11 16:46:14 +0200

    allow to choose the connection method
    Signed-off-by: honza <honza@inuits.eu>

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,23 +2,24 @@
 #
 # Initialization class for the pakiti service
 class pakiti (
-  $motd             = $pakiti::params::motd,
+  $motd              = $pakiti::params::motd,
 
-  $client           = $pakiti::params::client,
-  $server           = $pakiti::params::server,
+  $client            = $pakiti::params::client,
+  $server            = $pakiti::params::server,
 
-  $servers_name     = $pakiti::params::servers_name,
-  $server_url       = $pakiti::params::server_url,
-  $server_curl      = $pakiti::params::server_curl,
-  $server_tag       = $pakiti::params::server_tag,
+  $servers_name      = $pakiti::params::servers_name,
+  $server_url        = $pakiti::params::server_url,
+  $server_curl       = $pakiti::params::server_curl,
+  $server_tag        = $pakiti::params::server_tag,
 
-  $server_hostname  = $pakiti::params::server_hostname,
-  $server_dbname    = $pakiti::params::server_dbname,
-  $server_username  = $pakiti::params::server_username,
-  $server_password  = $pakiti::params::server_password,
+  $server_hostname   = $pakiti::params::server_hostname,
+  $server_dbname     = $pakiti::params::server_dbname,
+  $server_username   = $pakiti::params::server_username,
+  $server_password   = $pakiti::params::server_password,
 
-  $server_web_url   = $pakiti::params::server_web_url,
-  $server_web_title = $pakiti::params::server_web_title,
+  $server_web_url    = $pakiti::params::server_web_url,
+  $server_web_title  = $pakiti::params::server_web_title,
+  $connection_method = $pakiti::params::connection_method,
 ) inherits pakiti::params {
 
   if $motd {
--- manifests/params.pp
+++ manifests/params.pp
@@ -3,21 +3,23 @@
 # This class contains every parameter which will be used in this puppet module
 # for setting up the pakiti agent
 class pakiti::params {
-  $motd             = false
+  $motd              = false
 
-  $server           = false
-  $client           = true
+  $server            = false
+  $client            = true
 
-  $servers_name     = 'pakiti.server.com:443'
-  $server_url       = '/feed/'
-  $server_curl      = 'curl'
-  $server_tag       = $::environment
+  $servers_name      = 'pakiti.server.com:443'
+  $server_url        = '/feed/'
+  $server_curl       = 'curl'
+  $server_tag        = $::environment
 
-  $server_hostname  = 'localhost'
-  $server_dbname    = 'pakiti'
-  $server_username  = 'pakiti_server'
-  $server_password  = 'mysql_password'
+  $server_hostname   = 'localhost'
+  $server_dbname     = 'pakiti'
+  $server_username   = 'pakiti_server'
+  $server_password   = 'mysql_password'
 
-  $server_web_url   = 'https://pakiti.cern.ch'
-  $server_web_title = 'My_Organization'
+  $server_web_url    = 'https://pakiti.cern.ch'
+  $server_web_title  = 'My_Organization'
+
+  $connection_method = 'curl'
 }
